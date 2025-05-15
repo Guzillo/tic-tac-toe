1f477c7a1f9b8f98beddf108b5c5ad146e537776
guzillo <guzillo2077@gmail.com>
2025-05-15 17:13:48 +0200

    Add: logic and styling of start-restart button

--- scripts/ScreenController.js
+++ scripts/ScreenController.js
@@ -5,23 +5,37 @@ let gameController = GameController;
 const gameStatusH2 = document.getElementById('game-status');
 const cells = document.querySelectorAll('.cell');
 const currentPlayerSpan = document.getElementById('current-player-span');
+const startRestartButton = document.getElementById('start-restart-button');
+const startRestartButtonText = document.getElementById('start-restart-button-text');
+
+let hasGameStarted = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+    setButtonToStartMode();
+})
 
 cells.forEach(cell => {
-    cell.addEventListener('click', (event) => clickHandlerBoard(event));})
+    cell.addEventListener('click', (event) => clickHandlerBoard(event));
+})
+
 
 const clickHandlerBoard = (event) => {
+    if (!hasGameStarted) {
+        setButtonToRestartMode();
+    }
+    hasGameStarted = true;
     event.stopPropagation();
     const row = event.target.dataset.row;
     const col = event.target.dataset.col;
     gameController.playRound(row, col, 'P1', 'P2');
     updateScreen();
-    updateCurrentPlayerTourSpan();
-    updateGameResult();
 }
 
 const updateScreen = () => {
     clearCells(cells);
     fillCells(gameController.getMarkedBoard(), cells);
+    updateGameResult();
+    updateCurrentPlayerTourSpan();
 }
 
 const clearCells = (cells) => {
@@ -45,22 +59,48 @@ const fillCells = (markedBoard, cells) => {
 
 const updateCurrentPlayerTourSpan = () => {
     const player = gameController.getCurrentPlayerTour();
-    const playerName = player.getName();
-    const playerMark = player.getMark();
-    console.log(playerName)
-    console.log(playerMark)
+    const playerName = player ? player.getName() : '';
+    const playerMark = player ? player.getName() : '';
     currentPlayerSpan.classList.add(playerMark.toUpperCase() === 'O' ? 'x' : 'o');
     currentPlayerSpan.textContent = playerName;
 }
 
 const updateGameResult = () => {
     const gameStatus = gameController.getCurrentGameStatus();
-    console.log(gameStatus);
     if (gameStatus === GameStatus.WIN) {
         gameStatusH2.textContent = gameController.evaluateGameStatus().player.getName() + " WON !!!";
     } else if (gameStatus === GameStatus.TIE) {
         gameStatusH2.textContent ="It's a TIE";
     }
+}
+
+startRestartButton.addEventListener('click', (event) => clickHandlerStartRestartButton(event));
+
+const clickHandlerStartRestartButton = (event) => {
+    if (hasGameStarted) {
+        hasGameStarted = !hasGameStarted;
+    }
+    event.stopPropagation();
+    gameController.resetGame();
+    updateScreen();
+    if (startRestartButton.classList.contains('start')) {
+        setButtonToRestartMode();
+    } else if (startRestartButton.classList.contains('restart')) {
+        setButtonToStartMode();
+    }
+}
+
+
+const setButtonToStartMode = () => {
+    startRestartButton.classList.remove('restart');
+    startRestartButton.classList.add('start');
+    startRestartButtonText.textContent = 'Start Game';
+}
+
+const setButtonToRestartMode = () => {
+    startRestartButton.classList.remove('start');
+    startRestartButton.classList.add('restart');
+    startRestartButtonText.textContent = 'Restart Game';
 }
 
 export default GameController;
